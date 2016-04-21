3c7d3dc59b6c2092df7405155d95f1c7402d9432
febriyola <febriyola.anastasia@gmail.com>
2016-04-21 09:38:37 +0700

    Fix bayar counter

--- app/src/main/java/pplb05/balgebun/app/AppConfig.java
+++ app/src/main/java/pplb05/balgebun/app/AppConfig.java
@@ -11,8 +11,4 @@ public class AppConfig {
 	public static String URL_CANCEL_ORDER = "http://aaa.esy.es/coba_wahid2/UpdateStatusPesananPenjual.php";
 	public static String URL_DELETE_ORDER = "http://aaa.esy.es/coba_wahid2/deletePesanan.php";
 	public static String URL_BAYAR_COUNTER = "http://aaa.esy.es/coba_wahid/bayar.php";
-	public static String URL_BATAL_ORDER = "http://aaa.esy.es/coba_wahid/batalPesanan.php";
-	public static String URL_LIST_USERNAME_BY_ROLE = "http://aaa.esy.es/coba_wahid/getUsersByRole.php";
-	public static String URL_UPDATE_PASSWORD = "http://aaa.esy.es/coba_wahid/updatePassword.php";
-	public static String URL_DELETE_USER = "http://aaa.esy.es/coba_wahid/deleteUser.php";
 }
