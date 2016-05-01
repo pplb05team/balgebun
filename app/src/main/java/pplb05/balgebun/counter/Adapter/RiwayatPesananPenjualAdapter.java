75f19ac628153330a0328bdae2b254d889afed33
dananarief <dananarief@gmail.com>
2016-05-01 10:56:47 +0700

    Kemarin ternyata beberapa kelas baru buatan saya belum ke add ke master
    add class
    RiwayatPesananPenjualAdapter.java
    RIwayatPesananPenjual.java
    RIwayatActivity.java
    activity_riwayat_antrian_penjual.xml
    penjual_riwayat_layout.xml

---
+++ app/src/main/java/pplb05/balgebun/counter/Adapter/RiwayatPesananPenjualAdapter.java
@@ -0,0 +1,67 @@
+package pplb05.balgebun.counter.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import pplb05.balgebun.R;
+import pplb05.balgebun.counter.Entity.RiwayatPesananPenjual;
+import pplb05.balgebun.counter.Fragment.RiwayatActivity;
+
+/**
+ * Created by dananarief on 27-04-16.
+ */
+public class RiwayatPesananPenjualAdapter extends BaseAdapter{
+    private ArrayList<RiwayatPesananPenjual> listRiwayat;
+    private Context context;
+    private TextView namaMakanan;
+    private TextView namaPembeli;
+    private TextView jumlahPesanan;
+    private TextView waktu;
+    RiwayatActivity frag;
+
+    public RiwayatPesananPenjualAdapter(ArrayList<RiwayatPesananPenjual> listRiwayat, Context context, RiwayatActivity frag) {
+        this.listRiwayat = listRiwayat;
+        this.context = context;
+        this.frag=frag;
+    }
+
+
+    @Override
+    public int getCount() {
+        return listRiwayat.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listRiwayat.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return listRiwayat.get(position).getPosition();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater i = LayoutInflater.from(context);
+        final View v = i.inflate(R.layout.penjual_riwayat_layout,parent,false);
+        namaMakanan = (TextView) v.findViewById(R.id.nama_menu);
+        namaPembeli = (TextView) v.findViewById(R.id.nama_pembeli);
+        jumlahPesanan = (TextView) v.findViewById(R.id.jumlah_pesanan);
+        waktu = (TextView) v.findViewById(R.id.tanggalPesanan);
+
+        namaMakanan.setText(listRiwayat.get(position).getNamaMakanan());
+        namaPembeli.setText(listRiwayat.get(position).getNamaPembeli());
+        jumlahPesanan.setText(Integer.toString(listRiwayat.get(position).getJumlahPesanan()));
+        waktu.setText(listRiwayat.get(position).getTanggal());
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/pplb05/balgebun/counter/Fragment/RiwayatActivity.java
@@ -0,0 +1,139 @@
+package pplb05.balgebun.counter.Fragment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import pplb05.balgebun.R;
+import pplb05.balgebun.counter.Adapter.RiwayatPesananPenjualAdapter;
+import pplb05.balgebun.counter.Entity.RiwayatPesananPenjual;
+
+public class RiwayatActivity extends Fragment {
+    private ArrayList<RiwayatPesananPenjual> riwayatPesanan;
+    private RiwayatPesananPenjualAdapter riwayatAdapter;
+    private String username;
+    private RequestQueue queue;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_riwayat_antrian_penjual, container, false);
+
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_riwayat_antrian_penjual);
+
+        SharedPreferences settings = getActivity().getSharedPreferences("BalgebunLogin", Context.MODE_PRIVATE);
+        username = settings.getString("username", "");
+
+        TextView counterUsernameText = (TextView) v.findViewById(R.id.counter_name_id_riwayat);
+        counterUsernameText.setText(username);
+
+        riwayatPesanan = new ArrayList<RiwayatPesananPenjual>();
+        //riwayatPesanan.add(new RiwayatPesananPenjual("tuti","Nasi Goreng",1,1,"2016-04-27 06:50:20",0));
+        //riwayatPesanan.add(new RiwayatPesananPenjual("tuti","Ayam Goreng",1,2,"2016-04-27 06:50:20",1));
+        //riwayatPesanan.add(new RiwayatPesananPenjual("adi","Nasi Goreng",1,3,"2016-04-27 06:50:20",2));
+        //riwayatPesanan.add(new RiwayatPesananPenjual("adi","Ayam Goreng",1,4,"2016-04-27 06:50:20",3));
+
+        getRiwayatList();
+
+        riwayatAdapter = new RiwayatPesananPenjualAdapter(riwayatPesanan,getActivity(),RiwayatActivity.this);
+        GridView riwayatList = (GridView) v.findViewById(R.id.riwayat_field);
+        riwayatList.setAdapter(riwayatAdapter);
+
+        riwayatAdapter.notifyDataSetChanged();
+        return v;
+    }
+
+
+    public void getRiwayatList(){
+        queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url = "http://aaa.esy.es/coba_wahid2/getRiwayatPesananPenjual.php";
+        final StringRequest stringChess = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("RESPONSE", "Get Pemesanan Response: " + response.toString());
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    Log.d("try", "try");
+                    if (!error) {
+                        riwayatPesanan.clear();
+                        Log.d("if", "" + error);
+                        String temp = jObj.getString("user");
+                        JSONArray menuTemp = new JSONArray(temp);
+                        Log.d("panjang", "" + menuTemp.length());
+                        for (int i = 0; i < menuTemp.length(); i++) {
+                            JSONObject jsonMenu = new JSONObject(menuTemp.get(i).toString());
+
+                                riwayatPesanan.add(new RiwayatPesananPenjual(
+                                        jsonMenu.getString("username_pembeli"),
+                                        jsonMenu.getString("nama_menu"),
+                                        Integer.parseInt(jsonMenu.getString("jumlah")),
+                                        Integer.parseInt(jsonMenu.getString("id"))
+                                        ,jsonMenu.getString("waktu")
+                                        , i)
+                                );
+                                Log.d("i=", "" + i);
+                                Log.d("menu", jsonMenu.getString("nama_menu") + "with id " + jsonMenu.getString("id"));
+
+                        }
+                        Log.d("panj", "" + riwayatPesanan.size());
+                        riwayatAdapter.notifyDataSetChanged();
+                        Log.d("DoNotify", "DoNotify");
+
+
+                        Log.d("ABCD", "ABCD");
+                    } else {
+                        //kalo database kosong
+                        riwayatPesanan.clear();
+                        riwayatAdapter.notifyDataSetChanged();
+                        Log.d("A", "A");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+                Log.d("hai", "hai");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                return params;
+            }
+
+        };
+
+        queue.add(stringChess);
+    }
+}
