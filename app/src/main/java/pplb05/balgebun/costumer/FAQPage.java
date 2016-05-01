66e97472f56ddd0e6bbae33f2d2df0a8354b5c63
HarisDwi <haris.dwi@ui.ac.id>
2016-05-01 14:13:54 +0700

    Add FAQ page

---
+++ app/src/main/java/pplb05/balgebun/costumer/FAQPage.java
@@ -0,0 +1,17 @@
+package pplb05.balgebun.costumer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pplb05.balgebun.R;
+
+/**
+ * Created by Haris Dwi Prakoso on 5/1/2016.
+ */
+public class FAQPage extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
