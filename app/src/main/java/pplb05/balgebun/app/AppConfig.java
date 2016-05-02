19cb347c9f7ec9efc3c86e17a0970169b40d530d
wahidnr <wahidnr1@gmail.com>
2016-05-02 08:59:42 +0700

    Fis image upload and gradle

--- app/src/main/java/pplb05/balgebun/app/AppConfig.java
+++ app/src/main/java/pplb05/balgebun/app/AppConfig.java
@@ -15,6 +15,7 @@ public class AppConfig {
 	public static String URL_BATAL_ORDER = "http://aaa.esy.es/coba_wahid/batalPesanan.php";
 	public static String URL_LIST_USERNAME_BY_ROLE = "http://aaa.esy.es/coba_wahid/getUsersByRole.php";
 	public static String URL_UPDATE_PASSWORD = "http://aaa.esy.es/coba_wahid/updatePassword.php";
+	public static String URL_UPDATE_PASSWORD_FROM_ADMIN = "http://aaa.esy.es/coba_wahid/updatePassword2.php";
 	public static String URL_DELETE_USER = "http://aaa.esy.es/coba_wahid/deleteUser.php";
 	public static String URL_UPDATE_PROFILE = "http://aaa.esy.es/coba_wahid/updateProfile.php";
 	public static String URL_DELETE_COUNTER = "http://aaa.esy.es/coba_wahid/deleteCounter.php";
